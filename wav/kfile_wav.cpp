e138afb53cb702bd4529e733dd12e89040cc6c89
Ryan Cumming <ryan@kde.org>
2002-01-20 03:40:10 +0000

    Add simple .WAV plugin
    svn path=/trunk/kdemultimedia/kfile-plugins/; revision=132625

---
+++ wav/kfile_wav.cpp
@@ -0,0 +1,111 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2002 Ryan Cumming <bodnar42@phalynx.dhs.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "kfile_wav.h"
+
+#include <kprocess.h>
+#include <klocale.h>
+#include <kgenericfactory.h>
+#include <kstringvalidator.h>
+#include <kdebug.h>
+
+#include <qdict.h>
+#include <qvalidator.h>
+#include <qcstring.h>
+#include <qfile.h>
+#include <qdatetime.h>
+
+#include <inttypes.h>
+
+typedef KGenericFactory<KWavPlugin> WavFactory;
+
+K_EXPORT_COMPONENT_FACTORY(kfile_wav, WavFactory( "kfile_wav" ));
+
+KWavPlugin::KWavPlugin(QObject *parent, const char *name,
+                       const QStringList &preferredItems)
+    
+    : KFilePlugin(parent, name, preferredItems)
+{
+}
+
+bool KWavPlugin::readInfo( KFileMetaInfo::Internal& info )
+{
+    QFile file(info.path());
+
+    uint16_t channel_count;
+    uint32_t sample_rate;
+    uint32_t bytes_per_second;
+    uint16_t bytes_per_sample;
+    uint32_t data_size;
+
+    if (!file.open(IO_ReadOnly))
+    {
+        kdDebug(7034) << "Couldn't open " << QFile::encodeName(info.path()) << endl;
+        return false;
+    }    
+
+    QDataStream dstream(&file);
+    // WAV headers are little-endian
+    dstream.setByteOrder(QDataStream::LittleEndian);
+
+    // Read the needed parts of the FORMAT chunk
+    file.at(22);
+    dstream >> channel_count;
+    dstream >> sample_rate;
+    dstream >> bytes_per_second;
+    dstream >> bytes_per_sample;
+
+    // And now go for the DATA chunk
+    file.at(40);
+    dstream >> data_size;
+
+    // These will cause as crash, besides being downright illgeal
+    if ((!channel_count) || (!bytes_per_second))
+        return false;
+
+    info.insert(KFileMetaInfoItem("Sample Size", i18n("Sample Size"),
+                QVariant(bytes_per_sample / channel_count * 8), false,
+                QString::null, i18n("bits")));
+
+    info.insert(KFileMetaInfoItem("Sample Rate", i18n("Sample Rate"),
+                QVariant(sample_rate), false,
+                QString::null, i18n("Hz")));
+
+    info.insert(KFileMetaInfoItem("Channels", i18n("Channels"),
+                QVariant(channel_count)));        
+
+    int wav_seconds = data_size / bytes_per_second;
+    int playmin = wav_seconds / 60;
+    int playsec = wav_seconds % 60;
+
+    QString str;
+    str = QString("%0:%1").arg(playmin)
+                          .arg(QString::number(playsec).rightJustify(2,'0') );
+
+    info.insert(KFileMetaInfoItem("Length", i18n("Length"),
+                QVariant(str), false, QString::null));
+    
+    info.setPreferredKeys(m_preferred);
+    info.setSupportedKeys(m_preferred);
+    info.setSupportsVariableKeys(false);
+    
+    return true;
+}
+
+#include "kfile_wav.moc"
---
+++ wav/kfile_wav.h
@@ -0,0 +1,38 @@
+/* This file is part of the KDE project
+ * Copyright (C) 2002 Ryan Cumming <bodnar42@phalynx.dhs.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef __KFILE_MP3_H__
+#define __KFILE_MP3_H__
+
+#include <kfilemetainfo.h>
+
+class QStringList;
+
+class KWavPlugin: public KFilePlugin
+{
+    Q_OBJECT
+    
+public:
+    KWavPlugin( QObject *parent, const char *name,
+                const QStringList& preferredItems );
+    
+    virtual bool readInfo( KFileMetaInfo::Internal& info );
+};
+
+#endif
