fdd410264657dde7eddeef92ddc57a3eed17dfb3
Scott Wheeler <wheeler@kde.org>
2003-10-27 09:26:31 +0000

    CVS_SILENT adding a copyright here
    svn path=/trunk/kdemultimedia/kfile-plugins/; revision=262391

--- mp3/kfile_mp3.cpp
+++ mp3/kfile_mp3.cpp
@@ -1,6 +1,7 @@
 /* This file is part of the KDE project
  * Copyright (C) 2001, 2002 Rolf Magnus <ramagnus@kde.org>
  * Copyright (C) 2002 Ryan Cumming <bodnar42@phalynx.dhs.org>
+ * Copyright (C) 2003 Scott Wheeler <wheeler@kde.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
