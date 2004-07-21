cf38d76b5529e2539f4b3261ab88af42e4f3f587
Allan Sandfeld Jensen <kde@carewolf.com>
2004-07-21 09:28:05 +0000

    CVS-SILENT: Also update the headers
    svn path=/trunk/kdemultimedia/kfile-plugins/; revision=331410

--- mpc/kfile_mpc.h
+++ mpc/kfile_mpc.h
@@ -1,22 +1,25 @@
-/*  KFile MPC-plugin
-
-    Copyright (C) 2004 Allan Sandfeld Jensen <kde@carewolf.com>
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-    Boston, MA 02111-1307, USA.
-*/
+/* This file is part of the KDE project
+ * Copyright (C) 2004 Allan Sandfeld Jensen <kde@carewolf.com>
+ *
+ * Originally based upon the kfile_ogg plugin:
+ *  Copyright (C) 2001, 2002 Rolf Magnus <ramagnus@kde.org>
+ * Interfacing to TagLib is copied from kfile_mp3 plugin:
+ *  Copyright (C) 2003 Scott Wheeler <wheeler@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 #ifndef __KFILE_MPC_H__
 #define __KFILE_MPC_H__
